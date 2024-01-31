38678890b8e260276cb39e47e7e99504c4adf951
Adil Farooque <Adilfarq11@gmail.com>
2024-01-31 11:18:44 +0530

    Check user exsits by email or phone

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -1,9 +1,11 @@
 package repository
 
 import (
-	"os/user"
+	"errors"
 
+	"github.com/Adilfarooque/Footgo_Ecommerce/pkg/domain"
 	"github.com/Adilfarooque/Footgo_Ecommerce/pkg/repository/interfaces"
+	"github.com/Adilfarooque/Footgo_Ecommerce/pkg/utils/models"
 	"gorm.io/gorm"
 )
 
@@ -11,12 +13,36 @@ type userRepository struct {
 	DB *gorm.DB
 }
 
-func NewUserRepository (DB *gorm.DB)interfaces.UserRepository{
+func NewUserRepository(DB *gorm.DB) interfaces.UserRepository {
 	return &userRepository{
-		DB:DB,
+		DB: DB,
 	}
 }
 
-func (ur *userRepository) CheckUserExistsByEmail(email string)(*domain.user,error){
+func (ur *userRepository) CheckUserExistsByEmail(email string) (*domain.User, error) {
+	var user domain.User
+	res := ur.DB.Where(&domain.User{Email: email}).First(&user)
+	if res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, res.Error
+	}
+	return &user, nil
+}
+
+func (ur *userRepository) CheckUserExistsByPhone(phone string) (*domain.User, error) {
+	var user domain.User
+	res := ur.DB.Where(&domain.User{Phone: phone}).First(&user)
+	if res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, res.Error
+	}
+	return &user, nil
+}
+
+func (ur *userRepository) UserSignUp(user models.UserSignUp)(models.UserDetailsResponse,error){
 	
 }
