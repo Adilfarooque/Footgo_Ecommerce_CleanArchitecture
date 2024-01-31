cfa363be8555b5bc4ff22cbc5a65c24b6fb709d0
Adil Farooque <Adilfarq11@gmail.com>
2024-01-31 11:35:28 +0530

    Added UserSignUp DB section created

--- pkg/repository/interfaces/user.go
+++ pkg/repository/interfaces/user.go
@@ -1,6 +1,13 @@
 package interfaces
 
+import (
+	"github.com/Adilfarooque/Footgo_Ecommerce/pkg/domain"
+	"github.com/Adilfarooque/Footgo_Ecommerce/pkg/utils/models"
+)
 
-type UserRepository interface{
-
+type UserRepository interface {
+	CheckUserExistsByEmail(email string) (*domain.User, error)
+	CheckUserExistsByPhone(phone string) (*domain.User, error)
+	UserSignUp(user models.UserSignUp) (models.UserDetailsResponse,error)
+	
 }
--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -43,6 +43,13 @@ func (ur *userRepository) CheckUserExistsByPhone(phone string) (*domain.User, er
 	return &user, nil
 }
 
-func (ur *userRepository) UserSignUp(user models.UserSignUp)(models.UserDetailsResponse,error){
-	
+func (ur *userRepository) UserSignUp(user models.UserSignUp) (models.UserDetailsResponse, error) {
+	var SignupDetails models.UserDetailsResponse
+	err := ur.DB.Raw("INSERT INTO users(firstname,lastname,email,password,phone)VALUES(?,?,?,?,?)RETURNING id,firstname,lastname,email,password,phone", user.Firstname, user.Lastname, user.Email, user.Password, user.Phone).Scan(&SignupDetails).Error
+	if err != nil {
+		return models.UserDetailsResponse{}, err
+	}
+	return SignupDetails, nil
+
 }
+
